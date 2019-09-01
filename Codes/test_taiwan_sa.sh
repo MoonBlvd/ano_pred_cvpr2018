a84b9eea8f03937a92a0a8701ceb48f9290c03a0
MoonBlvd <brianyao@umich.edu>
2019-09-01 11:56:11 -0400

    prepare for A3D 2.0 experiments

--- Codes/test_taiwan_sa.sh
+++ Codes/test_taiwan_sa.sh
@@ -1,6 +1,6 @@
-python test_taiwan_sa.py -g 0 --dataset taiwan_sa -b 4 -i 1 --num_his 4 --test_folder /media/DATA/VAD_datasets/taiwan_sa/testing/frames/ --config training_hyper_params/hyper_params.ini --evaluate compute_auc
+#python test_taiwan_sa.py -g 0 --dataset taiwan_sa -b 4 -i 1 --num_his 4 --test_folder /media/DATA/VAD_datasets/taiwan_sa/testing/frames/ --config training_hyper_params/hyper_params.ini --evaluate compute_auc
 
-#python test_taiwan_sa.py -g 0 --dataset A3D -b 4 -i 1 --num_his 4 --test_folder /media/DATA/A3D/frames/ --config training_hyper_params/hyper_params.ini --evaluate compute_auc
+python test_taiwan_sa.py -g 0 --dataset A3D -b 4 -i 1 --num_his 4 --test_folder /media/DATA/A3D/frames/ --config training_hyper_params/hyper_params.ini --evaluate compute_auc
 
 #python test_taiwan_sa.py -g 0 --dataset ucf_crimes -b 4 -i 1 --num_his 4 --test_folder /media/DATA/VAD_datasets/UCF_Crimes/frames/testing/ --config training_hyper_params/hyper_params.ini --evaluate compute_auc
 
